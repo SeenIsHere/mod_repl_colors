091d1ff818e794a023fb543276e254d875a9ecf5
Seen The Bean <sdevine815@gmail.com>
2022-04-21 01:27:10 -0400

    Create blackout.js

---
+++ blackout.js
@@ -0,0 +1,6 @@
+var theme elements = 
+  document.querySelectorAll('.replit-ui-theme-root.dark') ??
+  document.querySelectorAll('.replit-ui-theme-root.light') ??
+  document.querySelectorAll('.replit-ui-theme-root.spooky')
+  
+theme.forEach(n=>{ n.style.setProperty('--background-root', 'lightblue')
