bbe6ba09a7af7ab44cd82a249c5f952881696c2e
Seen The Bean <sdevine815@gmail.com>
2022-04-21 01:39:07 -0400

    Update blackout.js

--- blackout.js
+++ blackout.js
@@ -1,8 +1,6 @@
 function main(){
 var theme elements = 
-  document.querySelectorAll('.replit-ui-theme-root.dark') ??
-  document.querySelectorAll('.replit-ui-theme-root.light') ??
-  document.querySelectorAll('.replit-ui-theme-root.spooky')
+  document.querySelectorAll('.replit-ui-theme-root.dark')
   
 theme.forEach(n=>{ n.style.setProperty('--background-root', 'lightblue')
 }
